7dd83843825d6e3dced46982d1908904248fe752
jignesh88 <jignesh.282@gmail.com>
2020-08-23 22:10:05 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Button from "@material-ui/core/Button";
+export default function App() {
+  return (
+    <div className="App">
+      <Button color="primary" variant="contained">
+        Click
+      </Button>
+    </div>
+  );
+}
